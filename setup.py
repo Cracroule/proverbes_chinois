c35aee147e69084096abc2e9a69a8bee5c8b4545
Cracroule <cracroule@gmail.com>
2019-10-06 12:17:35 +1300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+__author__ = 'rpil'
+
+from setuptools import setup, find_packages
+#from distutils.core import setup
+
+setup(
+    version='0.0.0.1',
+    name='proverbes_chinois',
+    description='Personal project about creating random chinese proverbs (in French)',
+    # license='rpil',
+    # package_dir={},
+    # packages=['perudo'],
+    packages=find_packages(),
+    #scripts=['scripts/tournament.py'],
+    install_requires=[],
+)
